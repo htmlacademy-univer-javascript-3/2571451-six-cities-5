befdc5171fde106e628ab9b7759b67604cb19e39
Mikhail Kozlov <wmikkoz@icloud.com>
2024-12-09 19:42:06 +0500

    add empty component for main page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,7 +4,11 @@ import Map from '@/components/map/map';
 import { OfferList } from '@/components/offer/list';
 import { AppRoute } from '@/const';
 import { setLoginRedirect } from '@/store/actions';
-import { useAppDispatch, useAppSelector } from '@/store/store';
+import {
+  placesEmptySelector,
+  useAppDispatch,
+  useAppSelector,
+} from '@/store/store';
 import { useEffect } from 'react';
 
 function MainMap() {
@@ -21,7 +25,24 @@ function MainMap() {
   );
 }
 
+function Empty() {
+  return (
+    <div className='cities__places-container cities__places-container--empty container'>
+      <section className='cities__no-places'>
+        <div className='cities__status-wrapper tabs__content'>
+          <b className='cities__status'>No places to stay available</b>
+          <p className='cities__status-description'>
+            We could not find any property available at the moment in Dusseldorf
+          </p>
+        </div>
+      </section>
+      <div className='cities__right-section'></div>
+    </div>
+  );
+}
+
 export default function Main() {
+  const placesEmpty = useAppSelector(placesEmptySelector);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -35,14 +56,18 @@ export default function Main() {
         <h1 className='visually-hidden'>Cities</h1>
         <CitySelector />
         <div className='cities'>
-          <div className='cities__places-container container'>
-            <OfferList />
-            <div className='cities__right-section'>
-              <section className='cities__map map'>
-                <MainMap />
-              </section>
+          {!placesEmpty ? (
+            <div className='cities__places-container container'>
+              <OfferList />
+              <div className='cities__right-section'>
+                <section className='cities__map map'>
+                  <MainMap />
+                </section>
+              </div>
             </div>
-          </div>
+          ) : (
+            <Empty />
+          )}
         </div>
       </main>
     </div>
